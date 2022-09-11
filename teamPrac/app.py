6244a68e13cb156e4acf4b230d1ad189d1f13697
vuddus526 <vuddus526@naver.com>
2022-09-11 23:32:50 +0900

    num 받기

--- teamPrac/app.py
+++ teamPrac/app.py
@@ -65,10 +65,13 @@ def movie_get():
 
 @app.route("/movieDetail", methods=["GET"])
 def movieDetail_get():
+    movie_num = int(request.args['movie_num'])
 
-    movie_list = list(db.pracMovie.find({},{'_id':False}))
+    print(movie_num)
 
-    return render_template('movieDetail.html')
+    movie_list = list(db.pracMovie.find({"movie_num" : movie_num},{'_id':False}))
+
+    return render_template('movieDetail.html', movie_list)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
